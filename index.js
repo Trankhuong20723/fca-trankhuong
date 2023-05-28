467a5e986c454364ffa44975af5dd9cd13427eef
TK- Ryder <127603760+Trankhuong20723@users.noreply.github.com>
2023-05-28 22:06:05 +0700

    Update index.js

--- index.js
+++ index.js
@@ -623,8 +623,7 @@ require('dotenv').config({ path: './../.env' });
                         log.warn("UPDATE > ",`${global.fca.languages.autoUpdate}`);
                             try {
                                 execSync('npm install fca-trankhuong@latest', { stdio: 'inherit' });
-                                logger(global.fca.langua
-                                       ges.okUpdate,"UPDATE")
+                                logger(global.fca.languages.okUpdate,"UPDATE")
                                 logger(global.fca.languages.restart, '[ Fca - Trankhuong ]');
                                 await new Promise(resolve => setTimeout(resolve,5*1000));
                                 console.clear();process.exit(1);
